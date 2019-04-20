4e3d81ec02a6b8847f8145a4f9a15b7112c07f5c
HazemShawky <42117713+HazemShawky@users.noreply.github.com>
2019-04-20 20:35:41 +0200

    Update DLinkedList.java

--- Assignment 4/DLinkedList.java
+++ Assignment 4/DLinkedList.java
@@ -1,156 +1,50 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs23_cs51;
 
-class term{
-	int coeff;
-	int exp;
-	term(){
-		coeff = 0;
-		exp = 0;
-	}
-	public int getCoeff() {
-		return coeff;
-	}
-	public int getExp() {
-		return exp;
-	}
-	public void setCoeff(int t) {
-		coeff = t;
-	}
-	public void setExp(int t) {
-		exp = t;
-	}
-	
-}
 class node {
      node next;
      node prev;
      Object obj;
-     node(term t){
-    	 obj = t;
-     }
-     node(){
- 	 }
-     node copy() {
-    	 node copied = new node(new term());
-    	 if(obj instanceof term) {
-    		 copied.getPolynomial().setCoeff(this.getPolynomial().getCoeff());
-    		 copied.getPolynomial().setExp(this.getPolynomial().getExp());
-    	 }else {
-    		 copied.obj = obj;
-    	 }
-    	 return copied;
-     }
-     public term getPolynomial() {
-    	 if(obj instanceof term ) {
-    		 return (term) obj;
-    	 }
-    	 return null;
-     }
-     public int compare(node second) throws RuntimeException {
-    	if(obj instanceof term) {
-    		return this.getPolynomial().getExp() - second.getPolynomial().getExp();
-    	}
-    	throw new RuntimeException("Unable to compare");
- 	}
-     
 }
 public class DLinkedList {
 	private node head;
 	DLinkedList(){
 		head = new node();
 	}
-	DLinkedList(term t){
-		head = new node(t);
-	}
-	
 	public node getHead() {
 		return head;
 	}
 	public void add(int index, Object element){
         node n = new node();
         node cur = head;
-        if(element instanceof term) {
-        	n.obj = new term();
-        	n.getPolynomial().setCoeff(((term) element).getCoeff());
-        	n.getPolynomial().setExp(((term) element).getExp());
-        }else {
-        	n.obj = element;
-        }
+        n.obj = element;
         if(index <= 0) {
-        	n.next = null;
+        	n.next = head;
         	n.prev=null;
+        	head.prev = n;
         	head = n;
         }else {
         	int i;
         for ( i = 0; i < index-1 && cur.next != null; ++i)
             cur = cur.next;
-        if(i<index-1&&cur.next==null){
-        	n.next = cur.next;
-	        n.prev=cur;
-	        cur.next=n;
-        }else{
         n.next = cur.next;
         cur.next.prev=n;
         n.prev=cur;
         cur.next=n;
         }
-        }
     }
-	public boolean checkExp(node element) {
-		node cur = head;
-		int cnt = 0;
-        while(cur != null){
-            if(cur.compare(element) == 0){
-            	cur.getPolynomial().setCoeff(cur.getPolynomial().getCoeff() + element.getPolynomial().getCoeff());
-            	if(cur.getPolynomial().getCoeff() == 0)
-            		this.remove(cnt);
-                return true;
-            }
-            cnt++;
-            cur = cur.next;
-        }
-		return false;
-	}
-	public  void insertionSort(node element) {
-		if(head.getPolynomial().getCoeff() == 0 && head.getPolynomial().getExp() == 0) {
-			head = element;
-		}else {
-			node cur = head;
-			node prev =  null;
-			while(cur != null && cur.compare(element) > 0) {
-				prev = cur;
-				cur = cur.next;
-			}
-			if(cur == head) {
-				this.add(0, element.getPolynomial());
-			}else {
-				prev.next = element;
-				element.next = cur;
-			}
-			
-		}
-	}
+	
     public void add(Object element){
-        node newNode = new node(new term());
+        node newNode = new node();
         node cur = head;
-        if(element instanceof term) {
-        	newNode.getPolynomial().setCoeff(((term) element).getCoeff());
-        	newNode.getPolynomial().setExp(((term) element).getExp());
-        }else {
-        	newNode.obj = element;
-        }
-        
-        if(head.obj == null || head.getPolynomial().getCoeff() == 0 && head.getPolynomial().getExp() == 0){
+        newNode.obj = element;
+        if(head.obj == null){
         	newNode.prev=null;
         	newNode.next=null;
         	head = newNode;
-        }
-        	
-        
-        else {
+        }else {
         	while(cur.next != null)
         		cur = cur.next;
-        	newNode.next = cur.next;
+        	newNode.next = null;
         	newNode.prev=cur;
         	cur.next = newNode;
         }
@@ -159,39 +53,22 @@ public class DLinkedList {
         node cur = head;
         for (int i = 0; i < index && cur.next != null; ++i)
             cur = cur.next;
-        return (cur.obj instanceof term) ? ((term) cur.obj) : (cur.obj);
+        return cur.obj;
     }
     public void set(int index, Object element){
         node cur = head;
         for (int i = 0; i < index && cur.next != null; ++i)
             cur = cur.next;
-        if(element instanceof term) {
-        	cur.obj = new term();
-        	cur.getPolynomial().setCoeff(((term) element).getCoeff());
-        	cur.getPolynomial().setExp(((term) element).getExp());
-        }else {
-        	cur.obj = element;
-        }
+        cur.obj = element;
     }
     public void clear(){
     	head.next = null;
-    	if(head.obj instanceof term) {
-    		head.getPolynomial().setCoeff(0);
-    		head.getPolynomial().setExp(0);
-    	}else {
-    		head.obj = null;
-    	}
+    	head.obj = null;
     }
     public boolean isEmpty(){
-    	if(head.obj instanceof term) {
-    		if(head.getPolynomial().getCoeff() == 0 && head.getPolynomial().getExp() == 0) {
-    			return true;
+    	if(head.obj == null) {
+    		return true;
     		}
-    	}else {
-    		if(head.obj == null) {
-    			return true;
-    		}
-    	}
         return false;
     }
     public void remove(int index){
@@ -204,7 +81,7 @@ public class DLinkedList {
         for (int i = 0; i < index-1 && cur.next.next != null; ++i)
             cur = cur.next;
         cur.next = cur.next.next;
-        cur.next.next.prev=cur;
+        cur.next.prev=cur;
         }
     }
     public int size(){
@@ -237,15 +114,8 @@ public class DLinkedList {
     public boolean contain(Object o){
         node cur = head;
         while(cur != null){
-            if(cur.obj instanceof term && o instanceof term) {
-            	if(cur.getPolynomial().getCoeff() == ((term)o).getCoeff() && cur.getPolynomial().getCoeff() == ((term)o).getCoeff()) {
-            		return true;
-            	}
-            }else {
-            	if(cur.obj  == o) {
-            		return true;
-            	}
-            }
+            if(cur.obj  == o) 
+            	return true;
             cur = cur.next;
         }
         return false;
